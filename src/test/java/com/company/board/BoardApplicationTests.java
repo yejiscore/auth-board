f0e2c426af2264c4b6586c7138912d7da3c2768c
yejiscore <yejisong24@gmail.com>
2025-05-01 13:51:15 +0900

    init: 초기 세팅

---
+++ src/test/java/com/company/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
