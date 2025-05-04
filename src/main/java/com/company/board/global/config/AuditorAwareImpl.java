ae828ca446055412d2fc10cad8c862cf8763aa84
yejiscore <yejisong24@gmail.com>
2025-05-05 02:47:25 +0900

    feat: JPA Auditing 기능 활성화

---
+++ src/main/java/com/company/board/global/config/AuditorAwareImpl.java
@@ -0,0 +1,15 @@
+package com.company.board.global.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        return Optional.of(1L); // 테스트용
+    }
+}
