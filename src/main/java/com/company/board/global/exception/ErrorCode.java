c4fafc30c3d69a45603d3328c4ff9f13d79b5d90
yejiscore <yejisong24@gmail.com>
2025-05-05 18:49:55 +0900

    feat: 에러 코드 추가

--- src/main/java/com/company/board/global/exception/ErrorCode.java
+++ src/main/java/com/company/board/global/exception/ErrorCode.java
@@ -9,10 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 1. 공통
-    SUCCESS(HttpStatus.OK, "000", "성공적으로 처리됐습니다."),
+    SUCCESS(HttpStatus.OK, "000", "성공적으로 처리되었습니다."),
     NEED_LOGIN(HttpStatus.UNAUTHORIZED, "001", "로그인이 필요합니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "002", "요청값이 유효하지 않습니다."),
     ENUM_TYPE_INVALID(HttpStatus.BAD_REQUEST, "003", "요청한 ENUM 값이 유효하지 않습니다."),
+    NO_CHANGES_DETECTED(HttpStatus.BAD_REQUEST, "004", "변경된 내용이 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "999", "서버 내부 오류가 발생했습니다."),
 
     // 2. Auth
@@ -25,6 +26,7 @@ public enum ErrorCode {
     USER_ONLY_ADMIN(HttpStatus.FORBIDDEN, "U100", "관리자만 가능한 기능입니다."),
     USER_ONLY_MASTER(HttpStatus.FORBIDDEN, "U200", "마스터만 가능한 기능입니다."),
     USER_ALREADY_WITHDRAWN(HttpStatus.BAD_REQUEST, "U300", "해당 회원은 이미 탈퇴하였습니다."),
+    USER_NICKNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "U400", "이미 사용 중인 닉네임입니다."),
 
     // 4. Post
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "P100", "존재하지 않는 게시글입니다."),
