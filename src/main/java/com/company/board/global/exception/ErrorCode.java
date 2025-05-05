fe82973e9ba19f11332393c5af1ad4b723d9d815
yejiscore <yejisong24@gmail.com>
2025-05-06 00:27:44 +0900

    feat: 댓글 에러 코드 추가

--- src/main/java/com/company/board/global/exception/ErrorCode.java
+++ src/main/java/com/company/board/global/exception/ErrorCode.java
@@ -41,7 +41,9 @@ public enum ErrorCode {
     COMMENT_NOT_AUTHOR(HttpStatus.FORBIDDEN, "C200", "댓글 작성자가 아닙니다."),
     COMMENT_CONTENT_EMPTY(HttpStatus.BAD_REQUEST, "C300", "댓글 내용이 비어 있습니다."),
     COMMENT_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "C400", "해당 댓글은 이미 삭제되었습니다."),
-    COMMENT_FORBIDDEN(HttpStatus.FORBIDDEN, "C500", "해당 댓글을 조회할 권한이 없습니다.");
+    COMMENT_FORBIDDEN(HttpStatus.FORBIDDEN, "C500", "해당 댓글을 조회할 권한이 없습니다."),
+    COMMENT_POST_NOT_FOUND(HttpStatus.NOT_FOUND, "C600", "존재하지 않는 게시글입니다."),
+    COMMENT_POST_MISMATCH(HttpStatus.BAD_REQUEST, "C700", "요청한 게시글에 속한 댓글이 아닙니다.");
 
     private final HttpStatus status;
     private final String code;
